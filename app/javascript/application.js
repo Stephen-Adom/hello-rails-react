2d2f8eed2bae0ba2b9adb41994606d9813d48514
Stephen-Adom <stephen.alaska94@hotmail.com>
2023-07-10 14:28:09 +0000

    setup redux toolkit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,12 +5,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes.js";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
-		{/* <Provider store={store}>
-    </Provider> */}
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	</React.StrictMode>
 );
